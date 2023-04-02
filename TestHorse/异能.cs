8790bf5a6c19bb170a4e1d10303752f3b2aee573
未来菌 <1045740922@qq.com>
2023-04-03 00:25:03 +0800

    test: 异能框架

---
+++ TestHorse/异能.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    public class 异能
+    {
+        public static string 唯一名称;
+        public static string 标签;
+        public static int 优先级;
+        public static float 持续时间;
+        public static string 添加者;
+        public static 马 承担者;
+
+        public delegate void 添加时();
+        public delegate void 运行时();
+        public delegate void 移除时();
+    }
+}
---
+++ TestHorse/异能脚本/起跑冲刺.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    public class 起跑冲刺 : 异能
+    {
+        new string 唯一名称 = "起跑冲刺";
+        new string 标签 = "系统";
+        new int 优先级 = 100;
+        new float 持续时间 = -1;
+        new string 添加者 = "系统";
+        new 马 承担者;
+
+        public 起跑冲刺(马 对象)
+        {
+            承担者 = 对象;
+        }
+
+        void 添加时()
+        {
+            // 添加一个加速度 +24
+            承担者.当前加速度.修正组.Add(new 数据表.属性修正
+            {
+                标签组 = new[] { 唯一名称 }.ToList(),
+                优先级 = 优先级,
+                是加算 = true,
+                修正值 = 24,
+                剩余持续时间 = 持续时间
+            });
+        }
+
+        void 运行时()
+        {
+
+        }
+
+        void 移除时()
+        {
+            // 移除一个加速度
+            for (int i = 0; i < 承担者.当前加速度.修正组.Count; i++)
+            {
+                if (承担者.当前加速度.修正组[i].标签组.Contains(唯一名称))
+                {
+                    承担者.当前加速度.修正组.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
